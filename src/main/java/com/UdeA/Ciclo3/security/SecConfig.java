2ebd0db8cf32855d2f98c4e1cf5d89fc4c307be8
Cecilio José Avila Ramos <110960338+CeciliojoseAvila@users.noreply.github.com>
2022-09-25 21:23:02 -0500

    actualizado para heroku

--- src/main/java/com/UdeA/Ciclo3/security/SecConfig.java
+++ src/main/java/com/UdeA/Ciclo3/security/SecConfig.java
@@ -31,6 +31,7 @@ public class SecConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/","VerEmpresas/**").hasRole("ADMIN")
